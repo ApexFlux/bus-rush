b4dc8847735ba793bbbc3c52fc94b98483f3e6fb
Anik <anik.das005dhaka@gmail.com>
2026-06-16 23:28:45 +0600

    Added platform to level 1

--- scene/characters/player.gd
+++ scene/characters/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -350.0
+const JUMP_VELOCITY = -300.0
 
 @onready var anim = $AnimationPlayer
 @onready var sprite = $AnimatedSprite2D # Grab this once to keep code clean
