4b6dfb3112a54fa4dccf3877b2ddf78bf0bd9d88
Anik <anik.das005dhaka@gmail.com>
2026-06-21 00:31:14 +0600

    added bus, finalllyyyyyy

--- scene/level_finished_screen.gd
+++ scene/level_finished_screen.gd
@@ -25,5 +25,5 @@ func _on_nextlevel_pressed() -> void:
 
 
 func _on_quit_pressed() -> void:
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://scene/menu.tscn")
 	pass # Replace with function body.
